f68680313e492391a6a049b2191ebb974608c3af
rietzche <haritzrahman98@gmail.com>
2018-02-22 18:03:04 +0700

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'rooms#show'
-
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
